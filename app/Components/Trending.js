76b1cb957934a56ebf2ec65b0741f49534bb27f6
Shubhbham <90957217+shubhbham@users.noreply.github.com>
2023-11-16 21:05:48 +0530

    Update Trending.js

--- app/Components/Trending.js
+++ app/Components/Trending.js
@@ -61,6 +61,7 @@ export default function Home() {
 
   const handleTypeChange = (event, videoId) => {
     event.preventDefault();
+    setSelectedType(event.target.value);
     setSelectedVideoId(videoId);
   };
 
